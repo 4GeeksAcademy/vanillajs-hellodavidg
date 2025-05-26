b6b43774e341d6a6f548d3f30fea90d00966d567
daviidgodino <david.godino@hotmail.com>
2025-05-26 10:51:56 +0000

    hecho

--- src/app.js
+++ src/app.js
@@ -9,3 +9,14 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+for (let p of pronoun) {
+  for (let a of adj) {
+    for (let n of noun) {
+      console.log(p + a + n + '.com');
+    }
+  }
+}
